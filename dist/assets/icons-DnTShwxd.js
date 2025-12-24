ca39deb53c9f7c01a9298abfc187eca99e02e8a6
Ryan <lcfryan.20@gmail.com>
2025-12-25 01:53:49 +0900

    t

--- dist/assets/icons-DnTShwxd.js
+++
@@ -1,1 +0,0 @@
-import"./vendor-BcR4xW8P.js";
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {Routes, Route} from "react-router-dom";
-// import Home from "./Home";
+import Home from "./Home";
 import Course_Recommender from "./Course_Recommender";
 import Cifar from "./Cifar";
 
@@ -7,7 +7,7 @@ import Cifar from "./Cifar";
 function App() {
   return (
     <Routes>
-      <Route path="/ryan_portfolio" element={<Course_Recommender />} />
+      <Route path="/ryan_portfolio" element={<Home />} />
       <Route path="/ryan_portfolio/course-recommender" element={<Course_Recommender />} />
       <Route path="/ryan_portfolio/cifar-gan" element={<Cifar />} />
     </Routes>
