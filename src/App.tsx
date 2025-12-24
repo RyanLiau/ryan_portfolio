b2505b9ec6fc4733a31ec67f165fdca5bfcbabe3
Ryan <lcfryan.20@gmail.com>
2025-12-25 04:29:05 +0900

    e

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import {Routes, Route} from "react-router-dom";
-import Home from "./Home";
-import Course_Recommender from "./Course_Recommender";
-import Cifar from "./Cifar";
+import {Outlet} from "react-router-dom";
+// import Home from "./Home";
+// import Course_Recommender from "./Course_Recommender";
+// import Cifar from "./Cifar";
 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/ryan_portfolio" element={<Home />} />
-      <Route path="/ryan_portfolio/course-recommender" element={<Course_Recommender />} />
-      <Route path="/ryan_portfolio/cifar-gan" element={<Cifar />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/ryan_portfolio" element={<Home />} />
+    //   <Route path="/ryan_portfolio/course-recommender" element={<Course_Recommender />} />
+    //   <Route path="/ryan_portfolio/cifar-gan" element={<Cifar />} />
+    // </Routes>
+    <nav>
+        <Outlet />
+    </nav>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Home from "./Home.tsx";
 import App from './App.tsx'
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Course_Recommender from './Course_Recommender.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/ryan_portfolio",
+    element: <App />,
+    children: [
+      {
+        path: "/ryan_portfolio",
+        element: <Home />,
+      },{
+        path: "/ryan_portfolio/course-recommender",
+        element: <Course_Recommender />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+        <RouterProvider router={router} />
     </StrictMode>
 )
+
