dfa71d6adc77e76e8f6b1690b493401977163976
Ryan <lcfryan.20@gmail.com>
2025-12-25 03:58:43 +0900

    try

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <BrowserRouter basename="/ryan_portfolio">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </StrictMode>
